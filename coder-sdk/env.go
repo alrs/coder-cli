a89511e765b0f627ba28aaec7619cf293f2df738
Guillaume J. Charmes <guillaume@coder.com>
2020-09-04 11:36:06 -0400

    Cleanup: - Scope errors when possible. - Fix nil pointers in unmarshal. - Align struct tags for readability. - Explicitly discard errors with a comment on why. - Import package order. - Fmt verb for types. - More comments.
    Signed-off-by: Guillaume J. Charmes <guillaume@coder.com>

--- coder-sdk/env.go
+++ coder-sdk/env.go
@@ -29,13 +29,18 @@ type Environment struct {
 	UpdatedAt        time.Time        `json:"updated_at"         tab:"-"`
 	LastOpenedAt     time.Time        `json:"last_opened_at"     tab:"-"`
 	LastConnectionAt time.Time        `json:"last_connection_at" tab:"-"`
-	AutoOffThreshold xjson.Duration   `json:"auto_off_threshold" tab:"-"`
+	AutoOffThreshold xjson.MSDuration `json:"auto_off_threshold" tab:"-"`
 }
 
 // RebuildMessage defines the message shown when an Environment requires a rebuild for it can be accessed.
 type RebuildMessage struct {
-	Text     string `json:"text"`
-	Required bool   `json:"required"`
+	Text             string           `json:"text"`
+	Required         bool             `json:"required"`
+	AutoOffThreshold xjson.MSDuration `json:"auto_off_threshold" tab:"-"`
+	RebuildMessages  []struct {
+		Text     string `json:"text"`
+		Required bool   `json:"required"`
+	} `json:"rebuild_messages" tab:"-"`
 }
 
 // EnvironmentStat represents the state of an environment
@@ -51,9 +56,7 @@ type EnvironmentStat struct {
 	DiskUsed        int64             `json:"disk_used"`
 }
 
-func (e EnvironmentStat) String() string {
-	return string(e.ContainerStatus)
-}
+func (e EnvironmentStat) String() string { return string(e.ContainerStatus) }
 
 // EnvironmentStatus refers to the states of an environment.
 type EnvironmentStatus string
@@ -67,7 +70,7 @@ const (
 	EnvironmentUnknown  EnvironmentStatus = "UNKNOWN"
 )
 
-// CreateEnvironmentRequest is used to configure a new environment
+// CreateEnvironmentRequest is used to configure a new environment.
 type CreateEnvironmentRequest struct {
 	Name     string   `json:"name"`
 	ImageID  string   `json:"image_id"`
@@ -81,50 +84,39 @@ type CreateEnvironmentRequest struct {
 
 // CreateEnvironment sends a request to create an environment.
 func (c Client) CreateEnvironment(ctx context.Context, orgID string, req CreateEnvironmentRequest) (*Environment, error) {
-	var env *Environment
-	err := c.requestBody(
-		ctx,
-		http.MethodPost, "/api/orgs/"+orgID+"/environments",
-		req,
-		env,
-	)
-	return env, err
+	var env Environment
+	if err := c.requestBody(ctx, http.MethodPost, "/api/orgs/"+orgID+"/environments", req, &env); err != nil {
+		return nil, err
+	}
+	return &env, nil
 }
 
 // EnvironmentsByOrganization gets the list of environments owned by the given user.
 func (c Client) EnvironmentsByOrganization(ctx context.Context, userID, orgID string) ([]Environment, error) {
 	var envs []Environment
-	err := c.requestBody(
-		ctx,
-		http.MethodGet, "/api/orgs/"+orgID+"/members/"+userID+"/environments",
-		nil,
-		&envs,
-	)
-	return envs, err
+	if err := c.requestBody(ctx, http.MethodGet, "/api/orgs/"+orgID+"/members/"+userID+"/environments", nil, &envs); err != nil {
+		return nil, err
+	}
+	return envs, nil
 }
 
 // DeleteEnvironment deletes the environment.
 func (c Client) DeleteEnvironment(ctx context.Context, envID string) error {
-	return c.requestBody(
-		ctx,
-		http.MethodDelete, "/api/environments/"+envID,
-		nil,
-		nil,
-	)
+	return c.requestBody(ctx, http.MethodDelete, "/api/environments/"+envID, nil, nil)
 }
 
 // DialWsep dials an environments command execution interface
-// See github.com/cdr/wsep for details
+// See https://github.com/cdr/wsep for details.
 func (c Client) DialWsep(ctx context.Context, env *Environment) (*websocket.Conn, error) {
-	return c.dialWs(ctx, "/proxy/environments/"+env.ID+"/wsep")
+	return c.dialWebsocket(ctx, "/proxy/environments/"+env.ID+"/wsep")
 }
 
-// DialEnvironmentBuildLog opens a websocket connection for the environment build log messages
+// DialEnvironmentBuildLog opens a websocket connection for the environment build log messages.
 func (c Client) DialEnvironmentBuildLog(ctx context.Context, envID string) (*websocket.Conn, error) {
-	return c.dialWs(ctx, "/api/environments/"+envID+"/watch-update")
+	return c.dialWebsocket(ctx, "/api/environments/"+envID+"/watch-update")
 }
 
-// DialEnvironmentStats opens a websocket connection for environment stats
+// DialEnvironmentStats opens a websocket connection for environment stats.
 func (c Client) DialEnvironmentStats(ctx context.Context, envID string) (*websocket.Conn, error) {
-	return c.dialWs(ctx, "/api/environments/"+envID+"/watch-stats")
+	return c.dialWebsocket(ctx, "/api/environments/"+envID+"/watch-stats")
 }
